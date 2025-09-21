5c313993b41b7e25ffd7d66f71875ef981e089ee
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-09-21 23:29:43 +0530

    feat: Add SimpleTest component to demonstrate basic React functionality

---
+++ src/SimpleTest.tsx
@@ -0,0 +1,14 @@
+export default function SimpleTest() {
+  return (
+    <div style={{ 
+      backgroundColor: 'red', 
+      color: 'white', 
+      padding: '20px',
+      fontSize: '24px',
+      minHeight: '100vh'
+    }}>
+      <h1>SIMPLE TEST - If you see this, React is working</h1>
+      <p>This is a basic React component without any external dependencies</p>
+    </div>
+  );
+}
