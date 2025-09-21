e95e23e6b69d9028ac24bd8a07451c2c6a63e911
smirk-dev <suryansh.mishra.sm6@gmail.com>
2025-09-21 23:30:06 +0530

    refactor: Update main.tsx to use SimpleTest component and comment out App and ClerkProvider imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import SimpleTest from "./SimpleTest.tsx";
+// import App from "./App.tsx";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
+// import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Clerk Publishable Key");
-}
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error("Missing Clerk Publishable Key");
+// }
 
 createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-    <App />
-  </ClerkProvider>
+  <SimpleTest />
+  // <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  //   <App />
+  // </ClerkProvider>
 );
   
